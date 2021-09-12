08dd86a8cf7b06c70b1f8ac3d8633635462b2ba1
jmf223 <jmf223@lehigh.edu>
2021-09-12 01:03:37 +0000

    Jake added current client

--- p1/client/requests.cc
+++ p1/client/requests.cc
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <iostream>
 #include <openssl/rand.h>
+#include <openssl/rsa.h>
 #include <vector>
 
 #include "../common/contextmanager.h"
@@ -21,7 +22,22 @@ using namespace std;
 /// @param keyfile The name of the file to which the key should be written
 void req_key(int sd, const string &keyfile) {
   cout << "requests.cc::req_key() is not implemented\n";
-  // NB: These asserts are to prevent compiler warnings
+  // send request for key
+  // make sure key is of kblock length 
+  std::vector<uint8_t> sendKey = REQ_KEY;
+  while (sendKey.length() < LEN_RKBLOCK) { // add pad0 to end of vector
+    sendKey.push_back('\0'); // append a null character
+  }
+  if (send_reliably(sd, sendKey)) { // key request sent
+      std::vector<uint8_t> key = reliable_get_to_eof();
+      while (key.empty()) { // keep reading until response is found
+        key = reliable_get_to_eof();
+      }
+      if (keyfile.length == LEN_RSA_PUBKEY) { // make sure we get correct public key length
+        write_file(keyfile, key, 0); // write to keyfile
+      }
+  }
+  // NB: These asserts are to prevent compiler warnings (send reliable)
   assert(sd);
   assert(keyfile.length() > 0);
 }
@@ -34,7 +50,7 @@ void req_key(int sd, const string &keyfile) {
 /// @param pass    The password of the user doing the request
 void req_reg(int sd, RSA *pubkey, const string &user, const string &pass,
              const string &, const string &) {
-  cout << "requests.cc::req_reg() is not implemented\n";
+  auto res = send_cmd(sd, pubkey, REQ_REG, ablock_ss(user, pass));
   // NB: These asserts are to prevent compiler warnings
   assert(sd);
   assert(pubkey);
@@ -50,7 +66,7 @@ void req_reg(int sd, RSA *pubkey, const string &user, const string &pass,
 /// @param pass    The password of the user doing the request
 void req_bye(int sd, RSA *pubkey, const string &user, const string &pass,
              const string &, const string &) {
-  cout << "requests.cc::req_bye() is not implemented\n";
+  auto res = send_cmd(sd, pubkey, REQ_BYE, ablock_ss(user, pass));
   // NB: These asserts are to prevent compiler warnings
   assert(sd);
   assert(pubkey);
@@ -66,7 +82,7 @@ void req_bye(int sd, RSA *pubkey, const string &user, const string &pass,
 /// @param pass    The password of the user doing the request
 void req_sav(int sd, RSA *pubkey, const string &user, const string &pass,
              const string &, const string &) {
-  cout << "requests.cc::req_sav() is not implemented\n";
+  auto res = send_cmd(sd, pubkey, REQ_SAV, ablock_ss(user, pass));
   // NB: These asserts are to prevent compiler warnings
   assert(sd);
   assert(pubkey);
@@ -83,7 +99,8 @@ void req_sav(int sd, RSA *pubkey, const string &user, const string &pass,
 /// @param setfile The file whose contents should be sent
 void req_set(int sd, RSA *pubkey, const string &user, const string &pass,
              const string &setfile, const string &) {
-  cout << "requests.cc::req_set() is not implemented\n";
+  auto res = send_cmd(sd, pubkey, REQ_SET, ablock_sss(user, pass, setfile));
+  send_result_to_file(res, setfile + ".file.dat");
   // NB: These asserts are to prevent compiler warnings
   assert(sd);
   assert(pubkey);
@@ -103,6 +120,8 @@ void req_set(int sd, RSA *pubkey, const string &user, const string &pass,
 void req_get(int sd, RSA *pubkey, const string &user, const string &pass,
              const string &getname, const string &) {
   cout << "requests.cc::req_get() is not implemented\n";
+  std::vector<uint8_t> msg = send_cmd(sd, pubkey, REQ_GET, ablock_sss(user, pass, getname));
+  send_result_to_file(res, getname + ".file.dat");
   // NB: These asserts are to prevent compiler warnings
   assert(sd);
   assert(pubkey);
@@ -122,6 +141,8 @@ void req_get(int sd, RSA *pubkey, const string &user, const string &pass,
 void req_all(int sd, RSA *pubkey, const string &user, const string &pass,
              const string &allfile, const string &) {
   cout << "requests.cc::req_all() is not implemented\n";
+  std::vector<uint8_t> msg = send_cmd(sd, pubkey, REQ_ALL, ablock_sss(user, pass, allfile));
+  send_result_to_file(res, allfile + ".file.dat");
   // NB: These asserts are to prevent compiler warnings
   assert(sd);
   assert(pubkey);
@@ -129,3 +150,169 @@ void req_all(int sd, RSA *pubkey, const string &user, const string &pass,
   assert(pass.length() > 0);
   assert(allfile.length() > 0);
 }
+
+
+// helper functions
+/// Pad a vec with random characters to get it to size sz
+///
+/// @param v  The vector to pad
+/// @param sz The number of bytes to add
+///
+/// @returns true if the padding was done, false on any error
+bool padR(vec &v, size_t sz) {
+  int num = sz-v.size;
+  unsigned char *buf;
+  int bytes = RAND_bytes(buf, num);
+  v.push_back(buf);
+  return bytes;
+}
+
+/// Create unencrypted ablock contents from two strings
+///
+/// @param s1 The first string
+/// @param s2 The second string
+///
+/// @return A vec representing the two strings
+vec ablock_ss(const string &s1, const string &s2) {
+  std::vector<uint8_t> result;
+  // protocol.h s1 = user s2 = pass
+  if (s1.size() > LEN_UNAME || s2.size() > LEN_PASSWORD) 
+    return result;
+  result.push_back(s1.size());
+  result.push_back(s2.size());
+  result.push_back(NULL);
+  result.push_back(NULL);
+  result.push_back(s1);
+  result.push_back(s2);
+  return result;
+}
+
+/// Create unencrypted ablock contents from two strings
+///
+/// @param s1 The first string
+/// @param s2 The second string
+/// @param s3 The third string
+///
+/// @return A vec representing the two strings
+vec ablock_sss(const string &s1, const string &s2, const string &s3) {
+  std::vector<uint8_t> result;
+  if (s1.size() > LEN_UNAME || s2.size() > LEN_PASSWORD || s3.size() > LEN_PROFILE_FILE) 
+    return result;
+  result.push_back(s1.size());
+  result.push_back(s2.size());
+  result.push_back(s3.size());
+  result.push_back(NULL);
+  result.push_back(s1);
+  result.push_back(s2);
+  result.push_back(s3);
+}
+
+/// Check if the provided result vector is a string representation of ERR_CRYPTO
+///
+/// @param v The vector being compared to RES_ERR_CRYPTO
+///
+/// @returns true if the vector contents are RES_ERR_CRYPTO, false otherwise
+bool check_err_crypto(const vec &v) {
+  std::string str(vec.begin(), vec.end());
+  if (strncpy(str, RES_ERR_CRYPTO, 2) == 0) {
+    return true;
+  }
+  return false;
+}
+
+/// Send a message to the server, using the common format for secure messages,
+/// then take the response from the server, decrypt it, and return it.
+///
+/// Many of the messages in our server have a common form (@rblock.@ablock):
+///   - @rblock padR(enc(pubkey, "CMD".aeskey.length(@msg)))
+///   - @ablock enc(aeskey, @msg)
+///
+/// @param sd  An open socket
+/// @param pub The server's public key, for encrypting the aes key
+/// @param cmd The command that is being sent
+/// @param msg The contents of the @ablock
+///
+/// @returns a vector with the (decrypted) result, or an empty vector on error
+vec send_cmd(int sd, RSA *pub, const string &cmd, const vec &msg) {
+  std::vector<uint8_t> msgLen = msg.size;
+  rBlock.resize(LEN_RKBLOCK);
+
+  // build aBlock and encrypt
+  std::vector<uint8_t> key = create_aes_key();
+  EVP_CIPHER_CTX *aeskey = create_aes_context(key, true);
+  std::vector<uint8_t> aBlockEncrypt = aes_crypt_msg(aeskey, msg);
+
+  // build rBlock
+  std::vector<uint8_t> rBlock = cmd;
+  rBlock.push_back(aeskey);
+  rBlock.push_back(msgLen);
+  padR(rBlock);
+  // encrypt rBlock
+  std::vector<uint8_t> rBlockEncrypt(LEN_RKBLOCK);
+  int numBytes = RSA_public_encrypt(LEN_RKBLOCK, rBlock.data, rBlockEncrypt.data, pubkey, RSA_PKCS1_OAEP_PADDING); 
+  rBlockEncrypt.resize(numBytes);
+  // check if encryption happend and send result
+  if (aBlockEncrypt.size > 0 && rBlockEncrypt.size > 0) {
+    std::vector<uint8_t> result;
+    result.push_back(rBlockEncrypt);
+    result.push_back(aBlockEncrypt);
+    // send to server
+    if(send_reliably(sd,result)) {
+      std::vector<uint8_t> response = reliable_get_to_eof();
+      while (response.empty()) { // keep reading until response is found
+        response = reliable_get_to_eof();
+      }
+
+      // Check errors for decryption
+      if (check_err_cryptol(response)) {
+        cout << "Server could not decrypt @ablock"
+        cout << endl;
+        return std::vector<uint8_t> empty;
+      }
+      // decipher
+      aeskey = reset_aes_context(key, false);
+      std::vector<uint8_t> decrpytmsg = aes_crypt_msg(aeskey, response);
+      reclaim_aes_context(aeskey);
+
+      // error codes
+      std::string str(decrpytmsg.begin(), decryptmsg.end());
+      if (strncmp(msg, RES_OK, 2)) 
+        cout << "Successful execution\n";
+      else if (strncmp(msg, RES_ERR_LOGIN, 2)) {
+        cout << "Not a valid user\n";
+      }
+      else if (strncmp(msg, RES_ERR_LOGIN, 2)) {
+         cout << "Wrong password or user"
+      }
+      else if (strncmp(msg, RES_ERR_USER_EXISTS, 2)) {
+        cout << "User already exists\n";
+      }
+      else if (strncmp(msg, RES_ERR_REQ_FMT, 2)) {
+        cout << "Server unable to extract user or password from request\n";
+      }
+      else if (strncmp(msg, RES_ERR_NO_USER, 2)) {
+        cout << "Not a valid user\n";
+      }
+      else if (strncmp(msg, RES_ERR_NO_DATA , 2)) {
+        cout << "No data found on user\n";
+      }
+      else if (strncmp(msg, RES_ERR_SERVER, 2)) {
+        cout << "Error on server side\n";
+      }
+
+      // return decrpytmsg
+      return decrpytmsg;
+    }
+    else
+      return std::vector<uint8_t> empty;
+  }
+  else 
+    return std::vector<uint8_t> empty;
+}
+
+/// If a buffer consists of OKbbbbd+, where bbbb is a 4-byte binary integer
+/// and d+ is a string of characters, write the bytes (d+) to a file
+///
+/// @param buf      The buffer holding a response
+/// @param filename The name of the file to write
+void send_result_to_file(const vec &buf, const string &filename);
