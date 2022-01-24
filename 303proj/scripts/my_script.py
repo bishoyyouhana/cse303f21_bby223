2cd7ab509f816cd31661127f75439bc09f7a9cd3
Bishoy Youhana <youhanna58@gmail.com>
2022-01-24 11:53:26 -0500

    added a combined version of the project

---
+++ 303proj/scripts/my_script.py
@@ -0,0 +1,21 @@
+'''
+maybe just do a readme
+user
+    register new user
+    set user content
+    get user content
+    get all the users
+    save
+key value
+    set key
+    get key
+    upsert key
+    delete key
+    get all keys
+    get top keys
+    save
+
+map reduce
+
+shutdown
+'''
